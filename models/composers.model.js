40631d8a7c5034b10c0014f352b0ee4f17d666d4
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2023-04-04 15:47:52 -0500

    Add the timestamp to the backend
    Adds the timestamp column to the backend. This adds a createdAt and updatedAt column to the backend.
    - The createdAt column is set when the row is created
    - The updatedAt column is set when the row is updated.

--- models/composers.model.js
+++ models/composers.model.js
@@ -1,26 +1,22 @@
 const { SqlError } = require("mariadb");
 
 module.exports = (sequelize, Sequelize) => {
-  const Composers = sequelize.define(
-    "composers",
-    {
-      name: {
-        type: Sequelize.STRING,
-      },
-      birthyear: {
-        type: Sequelize.STRING,
-      },
-      deathyear: {
-        type: Sequelize.STRING,
-      },
-      id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        unique: true,
-      },
+  const Composers = sequelize.define("composers", {
+    name: {
+      type: Sequelize.STRING,
     },
-    { timestamps: false }
-  );
+    birthyear: {
+      type: Sequelize.STRING,
+    },
+    deathyear: {
+      type: Sequelize.STRING,
+    },
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true,
+    },
+  });
   return Composers;
 };
